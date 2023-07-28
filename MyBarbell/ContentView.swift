625dfb1b9de72d0bf4fed471df3729e5019f056e
jason goodney <jason.goodney@gmail.com>
2023-07-27 20:28:37 -0400

    Initial Commit

---
+++ MyBarbell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBarbell
+//
+//  Created by Jason Goodney on 7/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBarbell/MyBarbellApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBarbellApp.swift
+//  MyBarbell
+//
+//  Created by Jason Goodney on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MyBarbellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
